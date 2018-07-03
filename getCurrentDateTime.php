418b894bfa6e1a8cd16554bb93c36b0ee6ca2484
champentrustz <champ.pittaya1995@gmail.com>
2018-07-03 22:38:23 +0700

    add api

---
+++ getCurrentDateTime.php
@@ -0,0 +1,32 @@
+<?php
+
+date_default_timezone_set("Asia/Bangkok");
+
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Headers: X-Requested-With, content-type, access-control-alloworigin,
+access-control-allow-methods, access-control-allow-headers');
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "ge-ssru";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+$conn->set_charset("utf8");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$dateArray = array('date' => date("Y-m-d"),
+    'time' => date("h:i:s"));
+echo json_encode($dateArray);
+
+
+
+
+
+
+
+
